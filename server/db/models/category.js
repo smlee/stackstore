2cdbcdd8ba3228ad2939fa4bb0d0b448e62ddf48
smlee <sangmlee23@gmail.com>
2015-06-10 17:53:51 -0400

    category model fixed

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -23,4 +23,17 @@ var schema = new mongoose.Schema({
 	}
 });
 
+schema.methods.getChildren = function() {
+   return mongoose.model('Category').find({parent: this.id}).exec();
+};
+schema.methods.getSiblings = function() {
+   	// mongoose.model('Category').findOne({_id: this.parent}, function(err, result){
+       return this.constructor.find({parent: this.parent, _id: {$ne: this._id }}).exec();
+   	//});
+};
+
+schema.pre('save', function(){
+	this.updated_at = Date.now();
+});
+
 mongoose.model('Category', schema);
