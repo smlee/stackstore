98e54ee2da687552526b7c68c69ffd9831719920
smlee <sangmlee23@gmail.com>
2015-06-15 15:31:56 -0400

    edited logic

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -8,7 +8,7 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('LoginCtrl', function ($scope, AuthService, $state, CartFactory) {
+app.controller('LoginCtrl', function ($scope, $rootScope, AuthService, $state, CartFactory) {
 
     $scope.login = {};
     $scope.error = null;
@@ -24,27 +24,15 @@ app.controller('LoginCtrl', function ($scope, AuthService, $state, CartFactory) 
             // when the user is verified, create a new Cart object in the db
             // but first, check if that cartID already exists.  if it does, updated the cart
             AuthService.getLoggedInUser().then(function(user){
-                console.log('user exists', user)
                 if(user) {
                     // CartFactory.getOpenCarts(user._id).then(function (carts){
                     //     console.log('These are the open carts if they exist: ',carts);
                     // })
-                    CartFactory.getCarts(user._id).then(function(cart){
-                        console.log('this is the user\'s cart', cart)
-                        // if they have a cart, add to it from localStorage to database
-                        if(cart !== undefined){
-                            CartFactory.updateOrder(cart._id, CartFactory.getFromLocalStorage())
-                        } else {
-                            var newCart = CartFactory.getFromLocalStorage();
-                            newCart['user'] = user._id;
-                            CartFactory.createCart(newCart).then(function (result) {
-                                console.log(result);
-                            })
-                        }
-                    })
+                    console.log('line 31 login.js ',$rootScope.previousPath);
+                    //location.href = $rootScope.previousPath;
+                    CartFactory.syncCart(user._id)
                 } else {
                     if (localStorage.userCart !== undefined){
-                        console.log('inside inside insde')
                         CartFactory.createCart(CartFactory.getFromLocalStorage());
                         localStorage.clear();
                     }
