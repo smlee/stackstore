dfa00f8e0ee79f942cc2ba53502ce5ba14f24316
jnissenbaum18 <jnissenbaum@verizon.net>
2015-06-15 15:58:06 -0400

    Fixed slight merge conflict

--- browser/js/product/product.js
+++ browser/js/product/product.js
@@ -21,7 +21,6 @@ app.config(function ($stateProvider){
     });
 });
 
-<<<<<<< HEAD
 app.controller('ProductController', function ($scope, $stateParams, user, carts, ProductFactory, CartFactory, ReviewsFactory, AuthService){
 	console.log('this is stateParams', $stateParams.id);
 	console.log('this is just stateParams', $stateParams);
@@ -64,40 +63,4 @@ app.controller('ProductController', function ($scope, $stateParams, user, carts,
 		else CartFactory.addToLocalStorage(cart);
 
 	};
-=======
-app.controller('ProductController', function ($scope, $stateParams, user, carts, reviews, ProductFactory, CartFactory){
-    console.log('this is stateParams', $stateParams.id);
-    console.log('this is just stateParams', $stateParams);
-
-    $scope.cart = {
-        total: 0,
-        items: [],
-        quantity: 0
-    };
-    $scope.product;
-    $scope.reviews = reviews;
-
-
-    ProductFactory.getPicture($stateParams).then(function(prod) {
-        console.log('INSIDE! product', prod);
-        $scope.picture = prod;
-        $scope.product = prod;
-    });
-
-    // ProductFactory.getCart()
-    // .then(function (cart) {
-    // 	$scope.cart = cart
-    // }) 
-
-    $scope.addcart = function(product, qty){
-        // ProductFactory.addCart();
-        $scope.cart.total += $scope.product.price;
-        $scope.cart.quantity++;
-
-        var cart = {all_items: [{art: product, quantity: qty}], order_type: "order", paid: false };
-        if(user) CartFactory.pushItem(carts._id, cart)
-        else CartFactory.addToLocalStorage(cart);
-
-    };
->>>>>>> a42f60373a2929ac06658928e22f55ca67d6806c
 });
