997768e2e97de48b3e4dafb5a84c749abc5606c1
smlee <sangmlee23@gmail.com>
2015-06-16 17:54:54 -0400

    merge horror fix

--- browser/js/common/factories/Contact.js
+++ browser/js/common/factories/Contact.js
@@ -1,11 +1,17 @@
 app.factory('ContactFactory', function($http){
-	return {
-		sendNewContactInfo: function(userId, contactInfo){
-			console.log('inside sendNewContactInfo', contactInfo)
-			return $http.post('/api/contact', {params: {_id: userId, contact: contactInfo}})
-			.then(function(response){
-				return response.data;
-			})
-		}
-	}
+    return {
+        sendNewContactInfo: function(userId, contactInfo){
+            console.log('inside sendNewContactInfo', contactInfo)
+            return $http.post('/api/contact', {params: {_id: userId, contact: contactInfo}})
+                .then(function(response){
+                    return response.data;
+                })
+        },
+
+        getContacts: function(userId) {
+            return $http.get('api/contact/'+userId).then(function(contacts){
+                return contacts.data;
+            })
+        }
+    }
 })
