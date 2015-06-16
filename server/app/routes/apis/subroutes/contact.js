271f58198f0559ed4d28283d1d87590a6022b28d
smlee <sangmlee23@gmail.com>
2015-06-16 15:25:51 -0400

    get route added

--- server/app/routes/apis/subroutes/contact.js
+++ server/app/routes/apis/subroutes/contact.js
@@ -5,6 +5,13 @@ module.exports = router;
 var Contact = mongoose.model('Contact');
 var User = mongoose.model('User');
 
+router.get('/:id', function (req, res, next) {
+    User.findOne({_id: req.params.id}, 'contact').populate('contact').exec()
+        .then(function(contacts){
+            res.json(contacts)
+        }).then(null, next);
+});
+
 router.post('/', function (req, res, next){
 	Contact.create(req.body.params.contact, function (err, contact){
 		if(err) return next(err);
@@ -12,7 +19,7 @@ router.post('/', function (req, res, next){
 			if(err) return next(err);
 			user.contact.push(contact._id);
 			user.save(function(err, data){
-				if(err) return next(err)
+				if(err) return next(err);
 				res.send("contact successfully added to user");
 			});
 		});		
